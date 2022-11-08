c380aaedc43601cc2f951e5d4a5bb21bf8623d5b
MSI\KAI <nhanksd85@gmail.com>
2022-11-08 20:30:39 +0700

    Update Ex1

--- STM32PROJECT/Core/Src/main.c
+++ STM32PROJECT/Core/Src/main.c
@@ -64,6 +64,8 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 	//TODO EX1
+	//ABC
+	//123
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
